c8458a41956dab6da1f706021b11aa581d6f4d24
Luis Carlos Garcia <luigarcia1013@gmail.com>
2026-08-30 18:08:53 -0500

    Add: palindromic-substrings - submission-3

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-3.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        sp = "^#" + "#".join(s) + "#$"
+
+        n = len(sp)
+        r = [0] * n
+
+        center, right = 0, 0
+
+        for i in range(1, n - 1):
+            mirror = 2 * center - i
+
+            if i < right:
+                r[i] = min(r[mirror], r[right - i])
+
+            while sp[i + r[i] + 1] == sp[i - r[i] - 1]:
+                r[i] += 1
+
+            if r[i] >= r[center]:
+                center = i
+                right = i + r[i]
+
+        res = 0
+        for i in range(len(r)):
+            res += (r[i] + 1) // 2
+
+        return res
